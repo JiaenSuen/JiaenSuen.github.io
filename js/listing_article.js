59a1d2017cc53a7c1bd98530a723eebab845587c
JiaenSuen <john20050826@gmail.com>
2025-01-04 04:00:51 +0800

    commit
    cc

---
+++ js/listing_article.js
@@ -0,0 +1,28 @@
+
+document.addEventListener("DOMContentLoaded", async () => {
+    const articleList = document.getElementById("article-list");
+    const response = await fetch("data/blog_data.json");
+    const articles = await response.json();
+
+    articles.forEach(article => {
+        const articleElement = document.createElement("div");
+        articleElement.classList.add("article");
+    
+        articleElement.innerHTML = `
+            <div class="article-content">
+                <a href="${article.link}">
+                <h3>${article.title}</h3></a>
+                <p>${article.description}</p>
+                
+            </div>
+            <div class="article-image">
+                
+                    <img src="${article.image}" alt="${article.title}">
+                
+            </div>
+        `;
+    
+        articleList.appendChild(articleElement);
+    });
+    
+});
